f31608c0439b654795765891c65653b252fe91dc
Jozefov <mn.mastaf@gmail.com>
2025-09-10 11:20:11 +0200

    benchmark: publish only utils/, definitions.py, and __init__.py; ignore data/ and models/

--- benchmark/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .dreams_classifier import DreamsClassifier
-from .lit_classifier     import LitClassifier
-
-MODEL_REGISTRY = {
-    'dreams_classifier': DreamsClassifier,
-    'lit_classifier':    LitClassifier,
-    # add future models here...
-}
